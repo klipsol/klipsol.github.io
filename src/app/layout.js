d4a968f71997eba33086c4f6febeee57a93d2dfc
Amit Negi <amit.negi077@gmail.com>
2024-10-24 13:44:34 +0530

    Project Setup: initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Poppins } from 'next/font/google';
+import './globals.css';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  display: 'swap',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata = {
+  title: 'Dpanda | Direct from brands',
+  description:
+    'Dpanda offer Your Gateway to E-Commerce. Enable contextual E-commerce everywhere, within minutes. Publisher can turn their site into a micro-ecommerce site.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`
+        ${poppins.className}
+         antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
