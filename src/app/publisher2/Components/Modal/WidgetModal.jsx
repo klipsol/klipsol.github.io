8d9b3d90bc95c0f91dfcf3cadb47022473e26841
Amit Negi <amit.negi077@gmail.com>
2024-11-14 16:16:47 +0530

    Live Preview links added

---
+++ src/app/publisher2/Components/Modal/WidgetModal.jsx
@@ -0,0 +1,50 @@
+const WidgetModal = ({ url, onClose }) => {
+  return (
+    <div className="fixed top-0 left-0 right-0 min-h-screen bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white w-11/12 max-w-lg rounded-lg relative">
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M6 18L18 6M6 6l12 12"></path>
+          </svg>
+        </button>
+
+        <div className="p-4">
+          <a
+            className="absolute bottom-2 left-4 flex justify-center items-center mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-transform duration-300 ease-in-out transform hover:scale-105"
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit Website for full experience
+          </a>
+
+          <iframe
+            src={url}
+            width="100%"
+            height="600"
+            frameBorder="0"
+            title="Preview"
+            className="rounded-lg"
+            id="demosite-iframe"
+            allow="*"
+            allowFullScreen
+            loading="lazy"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WidgetModal;
--- src/app/publisher2/Components/Nav/Navbar.js
+++ src/app/publisher2/Components/Nav/Navbar.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import CalendlyModal from '../Calendly/CalendlyModal';
+import LivePreviewLinks from './LivePreviewLinks';
 
 export default function Navbar() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,7 +17,8 @@ export default function Navbar() {
         <div className="flex justify-between items-center flex-col md:flex-row bg-transparent rounded-3xl relative custom-text-base ">
           <img src="/DpandaLogo.png" alt="logo" className="max-h-10" />
           <div className="flex md:gap-x-6 items-center pt-4 w-full md:w-fit md:pt-0 justify-between">
-            <div className="flex flex-col cursor-pointer">
+            <LivePreviewLinks />
+            {/* <div className="flex flex-col cursor-pointer">
               <div className="flex gap-x-2 items-center relative">
                 <span className="bg-[#F7BA30]/80 w-2 h-2 rounded-full "></span>
                 <span className="bg-[#F7BA30] w-2 h-2 rounded-full animate-ping [animation-duration:0.9s] absolute"></span>
@@ -25,7 +27,7 @@ export default function Navbar() {
               <p className="text-[#F7BA30] text-[10px] text-center">
                 for pinterest
               </p>
-            </div>
+            </div> */}
             <button
               className="rounded-[24px] bg-secondaryBg px-4 py-1 text-white font-medium"
               variant="primary"
