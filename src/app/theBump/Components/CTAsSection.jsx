04af44705c9c9d75a12a3c3ea38688572442d876
maxxximast <purujit.negi@gmail.com>
2024-11-27 17:21:19 +0530

    grammer and spelling fixes along with pascal case

--- src/app/theBump/Components/CTAsSection.jsx
+++ src/app/theBump/Components/CTAsSection.jsx
@@ -16,7 +16,7 @@ const CTAsSection = () => {
 		<>
 			<div className="relative">
 				<p className="mt-4 mb-6">
-					3. Banner click can be customised to tigger multiple variations of the widget
+					3. Banner click can be customised to trigger multiple variations of the widget
 				</p>
 				<p className="my-2">3.1: Category or Collection of Products</p>
 				<img
