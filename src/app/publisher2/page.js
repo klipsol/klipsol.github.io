dbf91c0c1545b12b5c3bb370406007dab2fd0ec7
Amarjit <=>
2024-11-27 14:45:26 +0530

    pushed changes

--- src/app/publisher2/page.js
+++ src/app/publisher2/page.js
@@ -17,56 +17,22 @@ import { Testimonial } from "./Components/Testimonial/Testimonial";
 import CoreFeatures from "./Components/CoreFeatures/CoreFeatures";
 import HigherMonitization from "./Components/HigherMonitization/HigherMonitization";
 
-export const metadata = {
-  title: "Your Page Title",
-  metadataBase: new URL("https://test-dpanda.pages.dev"),
-  charset: "UTF-8",
-  viewport: "width=device-width, initial-scale=1.0",
-  description: "Monetize By Embedding Brands Within Your Site",
-  keywords: ["brand", "monetization", "publisher", "site"],
-  authors: [{ name: "Your Name" }],
+export async function generateMetadata({ params }) {
+  // Get the path from params
+  const path = params.slug ? params.slug.join("/") : "/";
 
-  openGraph: {
-    title: "Everything To Transact Brands On Your Site",
-    description: "Monetize By Embedding Brands Within Your Site",
-    url: "/publisher2",
-    siteName: "Dpanda",
-    images: [
-      {
-        url: "https://imagekit.dpanda.in/publisher-banner/1731586986_image.png",
-      },
-    ],
-    type: "website",
-  },
-
-  twitter: {
-    card: "summary_large_image",
-    title: "Everything To Transact Brands On Your Site",
-    description: "Monetize By Embedding Brands Within Your Site",
-    images: [
-      "https://imagekit.dpanda.in/publisher-banner/1731586986_image.png",
-    ],
-    site: "@yourtwitterhandle",
-    creator: "@yourtwitterhandle",
-  },
-
-  other: {
-    "linkedin:title": "Everything To Transact Brands On Your Site",
-    "linkedin:description": "Monetize By Embedding Brands Within Your Site",
-    "linkedin:image":
-      "https://imagekit.dpanda.in/publisher-banner/1731586986_image.png",
-
-    "whatsapp:title": "Everything To Transact Brands On Your Site",
-    "whatsapp:description": "Monetize By Embedding Brands Within Your Site",
-    "whatsapp:image":
-      "https://imagekit.dpanda.in/publisher-banner/1731586986_image.png",
-
-    "slack:title": "Everything To Transact Brands On Your Site",
-    "slack:description": "Monetize By Embedding Brands Within Your Site",
-    "slack:image":
-      "https://imagekit.dpanda.in/publisher-banner/1731586986_image.png",
-  },
-};
+  return {
+    title: "Innovative Digital Solutions for Modern Businesses",
+    description:
+      "Transforming digital landscapes with cutting-edge technology and strategic insights across multiple domains.",
+    openGraph: {
+      title: "Innovative Digital Solutions for Modern Businesses",
+      description:
+        "Transforming digital landscapes with cutting-edge technology and strategic insights across multiple domains.",
+      images: ["https://www.dummyimage.com/1200x630/"],
+    },
+  };
+}
 
 export default function Publisher2() {
   return (
