3a52a742dacf557bb63e61d6d330fc54e3241349
maxxximast <purujit.negi@gmail.com>
2024-11-28 14:47:24 +0530

    middleware to manage publisher corporate site subdomains

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+	const host = req.headers.get('host') || '';
+	const subdomain = host.split('.')[0];
+
+	if (subdomain && subdomain !== 'dpanda' && subdomain !== 'www') {
+		const url = req.nextUrl.clone();
+		url.pathname = `/${subdomain}${url.pathname}`;
+		return NextResponse.rewrite(url);
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
