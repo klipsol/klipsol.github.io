0cb5a519cdfc09325ac1be16898ed3528377b313
maxxximast <purujit.negi@gmail.com>
2024-11-28 20:54:11 +0530

    design tweaks

--- src/app/publisher2/Components/Hero.js/Hero.js
+++ src/app/publisher2/Components/Hero.js/Hero.js
@@ -1,224 +1,185 @@
-"use client";
-import { useScrollTo } from "@/app/hooks/useSmoothScroll";
-import { useRef } from "react";
-import DPVideo from "../DPVideo/DPVideo";
-import HighlightedText from "../HighlightedText/HighlightedText";
-import Navbar from "../Nav/Navbar";
+'use client';
+import { useScrollTo } from '@/app/hooks/useSmoothScroll';
+import { useRef } from 'react';
+import DPVideo from '../DPVideo/DPVideo';
+import HighlightedText from '../HighlightedText/HighlightedText';
+import Navbar from '../Nav/Navbar';
 
 export default function Hero() {
-  const scroll = useScrollTo();
-  const secondSectionRef = useRef(null);
-  const logoRef = useRef(null);
+	const scroll = useScrollTo();
+	const secondSectionRef = useRef(null);
+	const logoRef = useRef(null);
 
-  // const heroAnimation = () => {
-  //   const tl = gsap.timeline({
-  //     defaults: { duration: 2, ease: "power3.out" },
-  //   });
+	// const heroAnimation = () => {
+	//   const tl = gsap.timeline({
+	//     defaults: { duration: 2, ease: "power3.out" },
+	//   });
 
-  //   tl.fromTo(
-  //     ["#left", "#right", "#switch", "#videoDiv"],
-  //     {
-  //       opacity: 0,
-  //       x: function (index) {
-  //         // Left element moves from left, right from right, switch no horizontal movement
-  //         if (index === 0) return -500;
-  //         if (index === 1) return 500;
-  //         if (index === 3) return 1000;
-  //         return 0;
-  //       },
-  //       y: function (index) {
-  //         // Only switch moves vertically
-  //         return index === 2 ? 100 : 0;
-  //       },
-  //     },
-  //     {
-  //       opacity: 1,
-  //       x: 0,
-  //       y: 0,
-  //     },
-  //     "<" // This makes all animations start at the same time
-  //   );
-  // };
+	//   tl.fromTo(
+	//     ["#left", "#right", "#switch", "#videoDiv"],
+	//     {
+	//       opacity: 0,
+	//       x: function (index) {
+	//         // Left element moves from left, right from right, switch no horizontal movement
+	//         if (index === 0) return -500;
+	//         if (index === 1) return 500;
+	//         if (index === 3) return 1000;
+	//         return 0;
+	//       },
+	//       y: function (index) {
+	//         // Only switch moves vertically
+	//         return index === 2 ? 100 : 0;
+	//       },
+	//     },
+	//     {
+	//       opacity: 1,
+	//       x: 0,
+	//       y: 0,
+	//     },
+	//     "<" // This makes all animations start at the same time
+	//   );
+	// };
 
-  // useGSAP(() => {
-  //   gsap.registerPlugin(ScrollTrigger);
+	// useGSAP(() => {
+	//   gsap.registerPlugin(ScrollTrigger);
 
-  //   const featureElement = document.getElementById("feature");
-  //   const calculateEndPosition = () => {
-  //     if (featureElement) {
-  //       const featurePosition =
-  //         featureElement.getBoundingClientRect().top + window.scrollY; // Get absolute position in the DOM
-  //       return featurePosition + 100; // Add 100 pixels to the position
-  //     }
-  //     return 420; // Fallback value in case the feature element is not found
-  //   };
+	//   const featureElement = document.getElementById("feature");
+	//   const calculateEndPosition = () => {
+	//     if (featureElement) {
+	//       const featurePosition =
+	//         featureElement.getBoundingClientRect().top + window.scrollY; // Get absolute position in the DOM
+	//       return featurePosition + 100; // Add 100 pixels to the position
+	//     }
+	//     return 420; // Fallback value in case the feature element is not found
+	//   };
 
-  //   const logoPin = gsap.to(logoRef.current, {
-  //     y: 320,
-  //     x: -100,
-  //     scrollTrigger: {
-  //       pin: true,
-  //       trigger: logoRef.current,
-  //       start: "top 53%",
-  //       end: calculateEndPosition(),
-  //       scrub: true,
-  //       onLeave: () => {},
-  //     },
-  //   });
+	//   const logoPin = gsap.to(logoRef.current, {
+	//     y: 320,
+	//     x: -100,
+	//     scrollTrigger: {
+	//       pin: true,
+	//       trigger: logoRef.current,
+	//       start: "top 53%",
+	//       end: calculateEndPosition(),
+	//       scrub: true,
+	//       onLeave: () => {},
+	//     },
+	//   });
 
-  //   heroAnimation();
+	//   heroAnimation();
 
-  //   return () => {
-  //     logoPin.kill();
-  //   };
-  // });
+	//   return () => {
+	//     logoPin.kill();
+	//   };
+	// });
 
-  return (
-    <>
-      <section className="hidden md:block h-screen md:relative overflow-hidden bg-white">
-        <Navbar />
-        {/* yellow image layout  */}
+	return (
+		<>
+			<section className="hidden md:block h-screen md:relative overflow-hidden bg-white">
+				<Navbar />
+				{/* yellow image layout  */}
 
-        <img
-          src="/yellow-layout.svg"
-          className="absolute -top-[12%] 2xl:-top-[9%] w-full  z-10 lg:block sm:hidden"
-          alt="bg"
-        />
-        <svg
-          className=" sm:top-[-10%] absolute lg:hidden sm:z-10"
-          xmlns="http://www.w3.org/2000/svg"
-          width="1078.713"
-          height="416.878"
-          viewBox="0 0 1078.713 416.878"
-        >
-          <path
-            id="Path_36575"
-            data-name="Path 36575"
-            d="M-49.13,574.735S-29.67,645.921,110.9,646.53s170.4-52.1,372.1-71.795,348.709-16.1,421.44-58.38,125.137-26.21,125.137-26.21V229.657H-49.13Z"
-            transform="translate(49.13 -229.657)"
-            fill="#f7ba30"
-          />
-        </svg>
-        <div className="top-[61%] h-fit xxl:top-[70%] 2xl:top-[54%] z-50 pointer-events-none w-full sec-container absolute xxl:text-[1.4em] inset-0">
-          <a
-            href="/theBump"
-            className="flex flex-col cursor-pointer bg-[#023046] rounded-[46px] py-2 xxl:py-3 xxl:rounded-[50px] w-fit px-10  "
-          >
-            <div className="flex gap-x-2 items-center relative">
-              {/* <span className="bg-[#F7BA30]/80 w-2 h-2 rounded-full "></span> */}
-              <span className="bg-[#F7BA30] w-2 h-2 xxl:w-2.5 xxl:h-2.5 rounded-full animate-ping [animation-duration:0.9s] absolute top-2 md:top-2 md:-left-4"></span>
-              <span className="bg-[#F7BA30] w-2 h-2 xxl:w-2.5 xxl:h-2.5 rounded-full  absolute top-2 md:top-2 md:-left-4"></span>
-              <span className="text-white text-center font-medium ml-3 xxl:ml-3.5 md:m-auto ">
-                Live Preview
-              </span>
-            </div>
-            <p className="text-[#F7BA30] text-[10px] xxl:text-[0.7em] text-center ml-3 md:m-auto">
-              for pinterest
-            </p>
-          </a>
-        </div>
-        <div className="absolute inset-0 bg-transparent h-full w-full z-10 sec-container">
-          <div className="lg:h-[90%] sm:h-full 2xl:h-[95%] w-1/2 sm:w-full relative ">
-            <div className="text-primaryTextColor absolute bottom-0 ">
-              <h1 className="py-4 custom-text-sm-medium xxl:text-3xl sm:text-xl ">
-                <HighlightedText text={"Everything"} /> To Transact Brands On
-                Your Site Site
-              </h1>
-              <div className="flex gap-x-3 custom-text-xs ">
-                <div className="bg-[#FEF4D3] sm:text-center sm:py-2 sm:px-1 lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative">
-                  Direct Relationship <br />
-                  With Brands
-                  <div className="absolute lg:h-full bg-[#F7BB30]  sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
-                </div>
-                <div className="bg-[#FEF4D3] sm:text-center sm:py-2 sm:px-1 lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative">
-                  Complete Tech & <br /> Operations Support
-                  <div className="absolute lg:h-full bg-[#F7BB30]  sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
-                </div>
-                <div className="bg-[#FEF4D3] sm:text-center sm:py-2 sm:px-1 lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative">
-                  Plug & <br />
-                  Play Setup
-                  <div className="absolute lg:h-full bg-[#F7BB30]  sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+				<img
+					src="/yellow-layout.svg"
+					className="absolute top-[-50px] w-full  z-10 lg:block sm:hidden"
+					alt="bg"
+				/>
+				<svg
+					className=" sm:top-[-10%] absolute lg:hidden sm:z-10"
+					xmlns="http://www.w3.org/2000/svg"
+					width="1078.713"
+					height="416.878"
+					viewBox="0 0 1078.713 416.878"
+				>
+					<path
+						id="Path_36575"
+						data-name="Path 36575"
+						d="M-49.13,574.735S-29.67,645.921,110.9,646.53s170.4-52.1,372.1-71.795,348.709-16.1,421.44-58.38,125.137-26.21,125.137-26.21V229.657H-49.13Z"
+						transform="translate(49.13 -229.657)"
+						fill="#f7ba30"
+					/>
+				</svg>
 
-        <div className="absolute inset-0 sec-container lg:flex justify-between sm:z-20">
-          {/* left section */}
-          <div className="lg:z-10 sm:z-30">
-            <h2 className="text-white lg:mt-[20%] 2xl:mt-[15%] sm:mt-[85px] ">
-              <span className="text-white lg:text-[3.8em] 2xl:text-[4.5em] xxl:text-[5em] leading-[1.2em] pt-3">
-                <span className="text-[#023046] font-extrabold">Discovery</span>{" "}
-                Meets
-                <br /> Commerce
-              </span>
-            </h2>
+				<div className="absolute inset-0 md:max-w-[90%] sm:mx-w-[1150px] mr-[25px] ml-auto lg:flex justify-between sm:z-20">
+					{/* left section */}
+					<div className="lg:z-10 sm:z-30 relative">
+						<h2 className="text-white sm:mt-[25%] ">
+							<span className="text-white lg:text-[60px] leading-[1.1] pt-3">
+								<span className="text-[#023046] font-bold">Discovery</span> Meets
+								<br /> Commerce
+							</span>
+						</h2>
 
-            <p className="text-black mt-4 w-[80%] font-medium custom-text-sm-medium">
-              Power of discovery and commerce, as championed by Google, Amazon,
-              TikTok, and Meta
-            </p>
+						<p className="text-black mt-5 w-[85%] font-medium custom-text-sm-medium">
+							Power of discovery and commerce, as championed by Google, Amazon, TikTok, and Meta
+						</p>
 
-            {/* description */}
-            {/* <h2 className="custom-text-sm-medium text-[#023046] font-extrabold mt-[3%] sm:mt-5">
-              <HighlightedText
-                text="Drive"
-                wrapperClass="bg-[#FEF4D4] bottom-1"
-              />
-            </h2>
-            <div className="flex gap-x-4 mt-[5%] xxl:mt-[7%] justify-start text-primaryTextColor">
-              <div className="bg-[#FFF9E6] rounded-2xl lg:p-3 2xl:p-4 xxl:pt-8 lg:pt-4 whitespace-nowrap custom-text-sm relative 2xl:min-w-[188px] lg:w-[120px] sm:rounded-xl sm:w-[110px] sm:px-2 sm:py-2">
-                <h1 className="absolute lg:-top-[35%] xxl:top-[-50%] font-medium title left-4 text-[#9E8637] sm:-top-[30%] ">
-                  01
-                </h1>
-                <h1 className="font-medium custom-text-sm">
-                  User <br />
-                  Experience
-                </h1>
-              </div>
-              <div className="bg-[#FFF9E6] rounded-2xl font-medium lg:p-3 lg:pt-4 2xl:p-4 xxl:pt-8 whitespace-nowrap custom-text-sm 2xl:min-w-[188px] relative h-fit lg:w-[120px] sm:rounded-xl sm:w-[110px] sm:px-2 sm:py-2">
-                <h1 className="absolute lg:-top-[35%] xxl:top-[-50%] title left-4 text-[#9E8637] sm:-top-[30%]">
-                  02
-                </h1>
-                <h1 className="font-medium custom-text-sm ">
-                  Lifetime <br />
-                  Value
-                </h1>
-              </div>
-              <div className="bg-[#FFF9E6] rounded-2xl lg:p-3 2xl:p-4  xxl:pt-8 lg:pt-4 whitespace-nowrap custom-text-sm 2xl:min-w-[188px] relative w-fit h-fit lg:w-[120px] sm:rounded-xl sm:w-[110px] sm:px-2 sm:py-2">
-                <h1 className="absolute lg:-top-[35%] xxl:top-[-50%] font-medium title left-4 text-[#9E8637] sm:-top-[30%]">
-                  03
-                </h1>
-                <h1 className="font-medium custom-text-sm">
-                  First-Party <br />
-                  Data
-                </h1>
-              </div>
-            </div> */}
-          </div>
+						<div className="z-[99999] mt-[9.5%] mb-[60px] w-full sec-container relative xxl:text-[1.4em] inset-0">
+							<a
+								href="/theBump"
+								className="flex relative flex-col cursor-pointer bg-[#023046] rounded-[46px] py-2 xxl:py-3 xxl:rounded-[50px] w-fit px-10  "
+							>
+								<div className="absolute left-[35px] top-[15px]">
+									<span className="bg-[#F7BA30] w-2 h-2 xxl:w-2.5 xxl:h-2.5 rounded-full animate-ping [animation-duration:0.9s] absolute top-2 md:top-2 md:-left-4"></span>
+									<span className="bg-[#F7BA30] w-2 h-2 xxl:w-2.5 xxl:h-2.5 rounded-full  absolute top-2 md:top-2 md:-left-4"></span>
+								</div>
+								<div className="flex gap-x-2 items-center relative">
+									{/* <span className="bg-[#F7BA30]/80 w-2 h-2 rounded-full "></span> */}
 
-          {/* right section */}
-          <div className="lg:h-[90%] xxl:h-[95%] rounded-b-full  lg:w-[45%] bg-[#003045] sm:hidden lg:block">
-            <div className="flex justify-center items-center h-full">
-              <DPVideo
-                id="videoDiv"
-                className="h-[60%] w-fit object-cover"
-                src="/HeroVideo.mp4"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="lg:h-[90%] pb-10 xxl:h-[95%] rounded-b-full bg-red top-[25%] sm:-z-0 lg:w-[45%] bg-[#003045] sm:h-[40%] sm:w-full sec-container relative lg:hidden">
-          <div className="flex justify-center items-end  h-full">
-            <DPVideo
-              id="videoDiv"
-              className="lg:h-[60%] sm:h-[60%] w-fit "
-              src="/HeroVideo.mp4"
-            />
-          </div>
-        </div>
-      </section>
-    </>
-  );
+									<span className="text-white text-[16px] text-center font-normal ml-[3px]">
+										Live Preview
+									</span>
+								</div>
+								<p className="text-[#F7BA30] text-[12px] xxl:text-[0.7em] font-medium text-left ml-[3px]">
+									for pinterest
+								</p>
+							</a>
+						</div>
+						<div>
+							<p className="text-primaryTextColor text-[22px] font-medium mb-[30px]">
+								Big Tech Is Transforming Discovery To Commerce
+							</p>
+							<div className="inset-0 bg-transparent w-full z-10 sec-container">
+								<div className="text-primaryTextColor">
+									<h1 className="py-4 custom-text-sm-medium sm:text-[26px] ">
+										<HighlightedText text={'Bringing'} /> that Power to You
+									</h1>
+									<div className="flex gap-x-3 custom-text-xs ">
+										<div className="bg-[#FEF4D3] font-medium sm:text-center sm:py-[12px] sm:px-1 lg:px-5 rounded-lg relative">
+											Technology
+											<div className="absolute lg:h-full bg-[#F7BB30]  sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
+										</div>
+										<div className="bg-[#FEF4D3] font-medium sm:text-center sm:py-[12px] sm:px-1 lg:px-5 rounded-lg relative">
+											Operations
+											<div className="absolute lg:h-full bg-[#F7BB30]  sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
+										</div>
+										<div className="bg-[#FEF4D3] font-medium sm:text-center sm:py-[12px] sm:px-1 lg:px-5 rounded-lg relative">
+											Brand Supply
+											<div className="absolute lg:h-full bg-[#F7BB30]  sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
+										</div>
+										<div className="bg-[#FEF4D3] font-medium sm:text-center sm:py-[12px] sm:px-1 lg:px-5 rounded-lg relative">
+											Intelligence
+											<div className="absolute lg:h-full bg-[#F7BB30]  sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					{/* right section */}
+					<div className="lg:h-[85%] xxl:h-[90%] rounded-b-full  lg:w-[66%] bg-[#003045] sm:hidden lg:block">
+						<div className="flex justify-center items-center h-full">
+							<DPVideo id="videoDiv" className="h-[60%] w-fit object-cover" src="/HeroVideo.mp4" />
+						</div>
+					</div>
+				</div>
+				<div className="lg:h-[90%] pb-10 xxl:h-[95%] rounded-b-full bg-red top-[25%] sm:-z-0 lg:w-[45%] bg-[#003045] sm:h-[40%] sm:w-full sec-container relative lg:hidden">
+					<div className="flex justify-center items-end  h-full">
+						<DPVideo id="videoDiv" className="lg:h-[58%] sm:h-[58%] w-fit " src="/HeroVideo.mp4" />
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
--- src/app/publisher2/Components/Hero.js/HeroMobile.jsx
+++ src/app/publisher2/Components/Hero.js/HeroMobile.jsx
@@ -1,84 +1,84 @@
-"use client";
-import Navbar from "@/app/roi-calculator/Components/Navbar";
-import DPVideo from "../DPVideo/DPVideo";
-import HighlightedText from "../HighlightedText/HighlightedText";
+'use client';
+import Navbar from '@/app/roi-calculator/Components/Navbar';
+import DPVideo from '../DPVideo/DPVideo';
+import HighlightedText from '../HighlightedText/HighlightedText';
 
 const HeroMobile = () => {
-  return (
-    <div className="block md:hidden ">
-      <Navbar customBg="#eebe50" />
-      <div
-        className="absolute bg-[#eebe50] w-full min-h-[400px] top-2"
-        style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 85%)" }}
-      ></div>
+	return (
+		<div className="block md:hidden ">
+			<Navbar customBg="#eebe50" />
+			<div
+				className="absolute bg-[#eebe50] w-full min-h-[400px] top-2"
+				style={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 85%)' }}
+			></div>
 
-      <div className="text-primaryTextColor p-4 relative min-h-[400px]">
-        <h2 className="text-white mt-6 text-xl leading-8 w-1/2">
-          <span className="text-white title">
-            <span className="text-[#023046] font-extrabold">Discovery</span>{" "}
-            Meets <br /> Commerce
-          </span>
-        </h2>
-        <p className="font-medium w-1/2 mt-5 leading-5">
-          Benefits of Discovery +{" "}
-          <span className="font-semibold">Commerce </span> on TikTok, Meta,
-          Google & Amazon To You
-        </p>
-        <div className="flex gap-4 justify-between my-8 mt-0">
-          <DPVideo
-            id="videoDiv"
-            className="absolute right-4 bottom-0 max-w-[180px] border-4 border-[#023046] rounded-xl"
-            src="/HeroVideo2.mp4"
-          />
-        </div>
-        <div className="top-[280px]  pointer-events-auto text-sm z-30 w-full sec-container absolute  inset-0">
-          <a
-            href="/theBump"
-            className="flex flex-col z-[9999] cursor-pointer bg-[#023046] rounded-[46px] py-2 xxl:py-3 xxl:rounded-[50px] w-fit px-6 "
-            style={{ lineHeight: "1.5" }}
-          >
-            <div className="flex gap-x-2 items-center relative">
-              {/* <span className="bg-[#F7BA30]/80 w-2 h-2 rounded-full "></span> */}
-              <span className="bg-[#F7BA30] w-2 h-2 xxl:w-2.5 xxl:h-2.5 rounded-full animate-ping [animation-duration:0.9s] absolute top-2 md:top-2 md:-left-4"></span>
-              <span className="bg-[#F7BA30] w-2 h-2 xxl:w-2.5 xxl:h-2.5 rounded-full  absolute top-2 md:top-2 md:-left-4"></span>
-              <span className="text-white text-center font-medium ml-4 xxl:ml-3.5 md:m-auto ">
-                Live Preview
-              </span>
-            </div>
-            <p className="text-[#F7BA30] text-[10px] xxl:text-[0.7em] text-center ml-3 md:m-auto">
-              for pinterest
-            </p>
-          </a>
-        </div>
-      </div>
-      <div className="text-primaryTextColor p-4 pb-0 mb-10">
-        <p className="text-[#000000] w-[60%] text-lg font-medium mt-5">
-          Big Tech is Transforming Discovery to Commerce
-        </p>
-        <h1 className="custom-text-sm-medium mt-5 font-medium xxl:text-3xl sm:text-2xl">
-          <HighlightedText text={"Bringing"} /> That Power To You
-        </h1>
-        <div className="mt-4 grid grid-cols-2 gap-3 max-w-[80%] font-semibold">
-          <div className="text-base py-4 p-2 bg-[#FEF4D3] sm:text-center lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative rounded-b-xl">
-            Technology
-            <div className="absolute lg:h-full bg-[#F7BB30] sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
-          </div>
-          <div className="text-base py-4 p-2 bg-[#FEF4D3] sm:text-center lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative rounded-b-xl">
-            Operations
-            <div className="absolute lg:h-full bg-[#F7BB30] sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
-          </div>
-          <div className="text-base py-4 p-2 bg-[#FEF4D3] sm:text-center lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative rounded-b-xl">
-            Brand Supply
-            <div className="absolute lg:h-full bg-[#F7BB30] sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
-          </div>
-          <div className="text-base py-4 p-2 bg-[#FEF4D3] sm:text-center lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative rounded-b-xl">
-            Intelligence
-            <div className="absolute lg:h-full bg-[#F7BB30] sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="text-primaryTextColor p-4 relative min-h-[400px] flex">
+				<div>
+					<h2 className="text-white mt-6 text-xl leading-8">
+						<span className="text-white title">
+							<span className="text-[#023046] font-extrabold">Discovery</span> Meets <br /> Commerce
+						</span>
+					</h2>
+					<p className="font-medium mt-5 leading-5">
+						Benefits of Discovery + <span className="font-semibold">Commerce </span> on TikTok, Meta, Google
+						& Amazon To You
+					</p>
+				</div>
+				<div className="relative mt-0">
+					<DPVideo
+						id="videoDiv"
+						className="max-w-[180px] sm:max-w-[150px] p-[4px] bg-[#023046] rounded-xl"
+						src="/HeroVideo2.mp4"
+					/>
+				</div>
+				<div className="top-[280px]  pointer-events-auto text-sm z-30 w-full sec-container absolute  inset-0">
+					<a
+						href="/theBump"
+						className="flex flex-col z-[9999] cursor-pointer bg-[#023046] rounded-[46px] py-2 xxl:py-3 xxl:rounded-[50px] w-fit px-6 "
+						style={{ lineHeight: '1.5' }}
+					>
+						<div className="flex gap-x-2 items-center relative">
+							{/* <span className="bg-[#F7BA30]/80 w-2 h-2 rounded-full "></span> */}
+							<span className="bg-[#F7BA30] w-2 h-2 xxl:w-2.5 xxl:h-2.5 rounded-full animate-ping [animation-duration:0.9s] absolute top-2 md:top-2 md:-left-4"></span>
+							<span className="bg-[#F7BA30] w-2 h-2 xxl:w-2.5 xxl:h-2.5 rounded-full  absolute top-2 md:top-2 md:-left-4"></span>
+							<span className="text-white text-center font-medium ml-4 xxl:ml-3.5 md:m-auto ">
+								Live Preview
+							</span>
+						</div>
+						<p className="text-[#F7BA30] text-[10px] xxl:text-[0.7em] text-center ml-3 md:m-auto">
+							for pinterest
+						</p>
+					</a>
+				</div>
+			</div>
+			<div className="text-primaryTextColor p-4 pb-0 mb-10">
+				<p className="text-[#000000] w-[60%] text-lg font-medium mt-5">
+					Big Tech is Transforming Discovery to Commerce
+				</p>
+				<h1 className="custom-text-sm-medium mt-5 font-medium xxl:text-3xl sm:text-2xl">
+					<HighlightedText text={'Bringing'} /> That Power To You
+				</h1>
+				<div className="mt-4 grid grid-cols-2 gap-3 max-w-[80%] font-semibold">
+					<div className="text-base py-4 p-2 bg-[#FEF4D3] sm:text-center lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative rounded-b-xl">
+						Technology
+						<div className="absolute lg:h-full bg-[#F7BB30] sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
+					</div>
+					<div className="text-base py-4 p-2 bg-[#FEF4D3] sm:text-center lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative rounded-b-xl">
+						Operations
+						<div className="absolute lg:h-full bg-[#F7BB30] sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
+					</div>
+					<div className="text-base py-4 p-2 bg-[#FEF4D3] sm:text-center lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative rounded-b-xl">
+						Brand Supply
+						<div className="absolute lg:h-full bg-[#F7BB30] sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
+					</div>
+					<div className="text-base py-4 p-2 bg-[#FEF4D3] sm:text-center lg:p-5 lg:py-1.5 rounded-lg 2xl:py-2 relative rounded-b-xl">
+						Intelligence
+						<div className="absolute lg:h-full bg-[#F7BB30] sm:p 2xl:py-2 lg:w-1.5 sm:w-full sm:h-1.5 left-0 top-0"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HeroMobile;
