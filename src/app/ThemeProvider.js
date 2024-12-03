28a8f08f59674bae740fabd0db1b9dae82f71c57
maxxximast <purujit.negi@gmail.com>
2024-12-03 16:54:14 +0530

    changes

--- src/app/ThemeProvider.js
+++ src/app/ThemeProvider.js
@@ -16,6 +16,12 @@ const ThemeProvider = () => {
 				root.style.setProperty('--action-color', '142 45 129'); // RGB for #8a317d
 				root.style.setProperty('--brand-color', '8 19 56'); // RGB for #8a317d
 				break;
+			case '/the-bump':
+				root.style.setProperty('--primary-color', '255 255 255'); // RGB for #fff
+				root.style.setProperty('--secondary-color', '8 19 56'); // RGB for #081338
+				root.style.setProperty('--action-color', '142 45 129'); // RGB for #8a317d
+				root.style.setProperty('--brand-color', '8 19 56'); // RGB for #8a317d
+				break;
 			default:
 				root.style.setProperty('--primary-color', '247 186 48'); // RGB for #f7ba30
 				root.style.setProperty('--secondary-color', '0 48 69'); // RGB for #003045
