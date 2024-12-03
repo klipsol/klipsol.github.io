dab97557fde4fb18c83f3fab4f5ff9174c01f2b6
maxxximast <purujit.negi@gmail.com>
2024-12-03 20:23:00 +0530

    responsive reatures table

--- src/app/theBump/Components/FeatureTable.jsx
+++ src/app/theBump/Components/FeatureTable.jsx
@@ -1,40 +1,33 @@
-import React from "react";
-
-const FeaturesTable = ({ features, title = "", subtitle = "" }) => {
-  return (
-    <div className="max-w-3xl mx-auto p-4 rounded-2xl bg-white my-10">
-      <h4 className="text-2xl font-semibold">{title}</h4>
-      <h5 className="text-base font-medium">{subtitle}</h5>
-      {title && <div className="bg-[#04133a] h-[1px] mt-1 mb-4"></div>}
-      <table className="w-full my-4 border-separate border-spacing-0 rounded-lg overflow-hidden shadow-lg">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="text-left text-sm font-semibold text-gray-700 py-3 px-6">
-              Features
-            </th>
-            <th className="text-left text-sm font-semibold text-gray-700 py-3 px-6">
-              Benefits
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {features.map((item, index) => (
-            <tr
-              key={index}
-              className="bg-white hover:bg-gray-50 transition-colors duration-300"
-            >
-              <td className="text-left text-sm text-gray-600 py-3 px-6 border border-gray-200">
-                {item.feature}
-              </td>
-              <td className="text-left py-3 px-6 border border-gray-200">
-                {item.benefit}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+const FeaturesTable = ({ features, title = '', subtitle = '' }) => {
+	return (
+		<div className="max-w-3xl mx-auto p-4 rounded-2xl bg-white my-10">
+			<h4 className="text-2xl font-semibold">{title}</h4>
+			<h5 className="text-base font-medium">{subtitle}</h5>
+			{title && <div className="bg-[#04133a] h-[1px] mt-1 mb-4"></div>}
+			<div className="overflow-x-auto">
+				<table className="w-full my-4 border-separate border-spacing-0 rounded-lg overflow-hidden shadow-lg">
+					<thead className="bg-gray-100">
+						<tr>
+							<th className="text-left text-sm font-semibold text-gray-700 py-3 px-6">Features</th>
+							<th className="text-left text-sm font-semibold text-gray-700 py-3 px-6">Benefits</th>
+						</tr>
+					</thead>
+					<tbody>
+						{features.map((item, index) => (
+							<tr key={index} className="bg-white hover:bg-gray-50 transition-colors duration-300">
+								<td className="text-left text-sm text-gray-600 py-3 px-6 border border-gray-200">
+									{item.feature}
+								</td>
+								<td className="text-left text-sm text-gray-600 py-3 px-6 border border-gray-200">
+									{item.benefit}
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 };
 
 export default FeaturesTable;
