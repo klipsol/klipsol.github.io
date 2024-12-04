cef91ae586c1c10734d39c96ab4200768452f753
Amarjit <=>
2024-12-04 19:08:40 +0530

    handled

--- src/app/ThemeProvider.js
+++ src/app/ThemeProvider.js
@@ -1,55 +1,55 @@
-'use client';
+"use client";
 
-import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const ThemeProvider = () => {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	useEffect(() => {
-		const root = document.documentElement;
+  useEffect(() => {
+    const root = document.documentElement;
 
-		switch (pathname) {
-			case '/publisher/the-bump':
-				root.style.setProperty('--primary-color', '255 255 255'); // RGB for #fff
-				root.style.setProperty('--secondary-color', '8 19 56'); // RGB for #081338
-				root.style.setProperty('--action-color', '142 45 129'); // RGB for #8a317d
-				root.style.setProperty('--brand-color', '142 45 129'); // RGB for #8a317d
-				break;
-			case '/the-bump':
-				root.style.setProperty('--primary-color', '255 255 255'); // RGB for #fff
-				root.style.setProperty('--secondary-color', '8 19 56'); // RGB for #081338
-				root.style.setProperty('--action-color', '142 45 129'); // RGB for #8a317d
-				root.style.setProperty('--brand-color', '8 19 56'); // RGB for #8a317d
-				break;
-			case '/publisher/buzzfeed':
-				root.style.setProperty('--primary-color', '250 233 234'); // RGB for #fff
-				root.style.setProperty('--secondary-color', '0 0 0'); // RGB for #081338
-				root.style.setProperty('--action-color', '205 52 55'); // RGB for #8a317d
-				root.style.setProperty('--brand-color', '205 52 55'); // RGB for #8a317d
-				break;
-			case '/publisher/readers-digest':
-				root.style.setProperty('--primary-color', '255 255 255'); // RGB for #fff
-				root.style.setProperty('--secondary-color', '0 0 0'); // RGB for #081338
-				root.style.setProperty('--action-color', '205 52 55'); // RGB for #8a317d
-				root.style.setProperty('--brand-color', '205 52 55'); // RGB for #8a317d
-				break;
-			case '/publisher/gq':
-				root.style.setProperty('--primary-color', '249 245 236'); // RGB for #fff
-				root.style.setProperty('--secondary-color', '0 0 0'); // RGB for #081338
-				root.style.setProperty('--action-color', '198 161 73'); // RGB for #8a317d
-				root.style.setProperty('--brand-color', '198 161 73'); // RGB for #8a317d
-				break;
-			default:
-				root.style.setProperty('--primary-color', '247 186 48'); // RGB for #f7ba30
-				root.style.setProperty('--secondary-color', '0 48 69'); // RGB for #003045
-				root.style.setProperty('--action-color', '0 48 69'); // RGB for #003045
-				root.style.setProperty('--brand-color', '247 186 48'); // RGB for #003045
-				break;
-		}
-	}, [pathname]);
+    switch (pathname) {
+      case "/publisher/the-bump":
+        root.style.setProperty("--primary-color", "255 255 255"); // RGB for #fff
+        root.style.setProperty("--secondary-color", "8 19 56"); // RGB for #081338
+        root.style.setProperty("--action-color", "142 45 129"); // RGB for #8a317d
+        root.style.setProperty("--brand-color", "142 45 129"); // RGB for #8a317d
+        break;
+      case "/the-bump":
+        root.style.setProperty("--primary-color", "255 255 255"); // RGB for #fff
+        root.style.setProperty("--secondary-color", "8 19 56"); // RGB for #081338
+        root.style.setProperty("--action-color", "142 45 129"); // RGB for #8a317d
+        root.style.setProperty("--brand-color", "8 19 56"); // RGB for #8a317d
+        break;
+      case "/publisher/buzzfeed":
+        root.style.setProperty("--primary-color", "250 233 234"); // RGB for #fff
+        root.style.setProperty("--secondary-color", "0 0 0"); // RGB for #081338
+        root.style.setProperty("--action-color", "205 52 55"); // RGB for #8a317d
+        root.style.setProperty("--brand-color", "205 52 55"); // RGB for #8a317d
+        break;
+      case "/publisher/readers-digest":
+        root.style.setProperty("--primary-color", "255 255 255"); // RGB for #fff
+        root.style.setProperty("--secondary-color", "0 0 0"); // RGB for #081338
+        root.style.setProperty("--action-color", "205 52 55"); // RGB for #8a317d
+        root.style.setProperty("--brand-color", "205 52 55"); // RGB for #8a317d
+        break;
+      case "/publisher/gq":
+        root.style.setProperty("--primary-color", "249 245 236"); // RGB for #fff
+        root.style.setProperty("--secondary-color", "0 0 0"); // RGB for #081338
+        root.style.setProperty("--action-color", "198 161 73"); // RGB for #8a317d
+        root.style.setProperty("--brand-color", "198 161 73"); // RGB for #8a317d
+        break;
+      default:
+        root.style.setProperty("--primary-color", "247 186 48"); // RGB for #f7ba30
+        root.style.setProperty("--secondary-color", "0 48 69"); // RGB for #003045
+        root.style.setProperty("--action-color", "0 48 69"); // RGB for #003045
+        root.style.setProperty("--brand-color", "247 186 48"); // RGB for #003045
+        break;
+    }
+  }, [pathname]);
 
-	return null;
+  return null;
 };
 
 export default ThemeProvider;
