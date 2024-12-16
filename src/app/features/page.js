747a13edf2d2128a89ea084fa53676ec3672a108
Amarjit <=>
2024-12-16 18:31:43 +0530

    handled screen flicker

--- src/app/features/page.js
+++ src/app/features/page.js
@@ -1,10 +1,18 @@
-import Navbar from '../roi-calculator/Components/Navbar';
-import HighlightedText from '../the-bump/Components/HighlightedText/HighlightedText';
-import FeatureCards from './Components/FeatureCard';
+"use client";
+import { useEffect, useState } from "react";
+import Navbar from "../roi-calculator/Components/Navbar";
+import HighlightedText from "../the-bump/Components/HighlightedText/HighlightedText";
+import ScrollableNavbar from "../the-bump/Components/Nav/Nav2";
+import ThemeWrapper from "../Theme/ThemeWrapper";
+import FeatureCards from "./Components/FeatureCard";
 
 export default function features(params) {
+  const [publisher, setPublisher] = useState("");
+  useEffect(() => {
+    setPublisher(localStorage.getItem("publisher"));
+  }, []);
   return (
-    <main>
+    <ThemeWrapper publisher={publisher}>
       <Navbar />
       <div className="relative sec-container flex flex-col lg:flex-row gap-y-4 mb-4 sm:px-[15px] lg:px-0">
         <div className="mt-6 lg:mt-0 lg:min-h-screen lg:w-1/2 lg:fixed lg:top-0 lg:left-0">
@@ -12,11 +20,11 @@ export default function features(params) {
             <h1 className="lg:mb-10 mb-4 text-4xl lg:text-6xl title whitespace-nowrap">
               Go Live in as Little
               <br />
-              as{' '}
+              as{" "}
               <HighlightedText
                 text=" 2 Hours."
                 wrapperClass="bg-[#ffdf74] bottom-2 h-[35%]"
-              />{' '}
+              />{" "}
             </h1>
             <p className="lg:text-4xl text-2xl">Get loads of features.</p>
             <p className="lg:text-4xl text-2xl font-bold">
@@ -32,6 +40,7 @@ export default function features(params) {
           </p>
         </div>
       </div>
-    </main>
+      <ScrollableNavbar />
+    </ThemeWrapper>
   );
 }
--- src/app/publisher/[...publisherId]/page.js
+++ src/app/publisher/[...publisherId]/page.js
@@ -1,15 +1,17 @@
-import { publisherMetaData } from '@/app/Data/metaData';
-import Publisher2 from '@/app/the-bump/page';
+import { publisherMetaData } from "@/app/Data/metaData";
+import Publisher2 from "@/app/the-bump/page";
 
 export async function generateMetadata({ params, headers }) {
-	const publisher = await params;
-	const publisherName = publisher['publisherId'][0];
-	const metaData = publisherMetaData[publisherName] ?? {};
-	return {
-		...metaData,
-	};
+  const publisher = await params;
+  const publisherName = publisher["publisherId"][0];
+  const metaData = publisherMetaData[publisherName] ?? {};
+  return {
+    ...metaData,
+  };
 }
 
-export default function page() {
-	return <Publisher2 />;
+export default async function page({ params }) {
+  const publisher = await params;
+  const publisherName = publisher["publisherId"][0];
+  return <Publisher2 publisher={publisherName} />;
 }
