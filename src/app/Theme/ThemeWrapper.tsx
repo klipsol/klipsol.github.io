313f6fce888c4f3a07f9a37018d6374799a41d59
Amarjit <=>
2024-12-17 12:43:48 +0530

    handled theme issue

--- src/app/Theme/ThemeWrapper.tsx
+++ src/app/Theme/ThemeWrapper.tsx
@@ -1,15 +1,17 @@
-import themeconfig from '../../../Data/themeconfig.json';
+import themeconfig from "../../../Data/themeconfig.json";
 
-export default function ThemeWrapper({ children, className = '', publisher }) {
+export default function ThemeWrapper({ children, className = "", publisher }) {
   const publisherTheme = themeconfig[publisher] ?? themeconfig.default;
+
+  console.log({ publisher });
   return (
     <main
       style={
         {
-          '--primary-color': publisherTheme['primary-color'],
-          '--secondary-color': publisherTheme['secondary-color'],
-          '--action-color': publisherTheme['action-color'],
-          '--brand-color': publisherTheme['brand-color'],
+          "--primary-color": publisherTheme["primary-color"],
+          "--secondary-color": publisherTheme["secondary-color"],
+          "--action-color": publisherTheme["action-color"],
+          "--brand-color": publisherTheme["8 19 56"],
         } as React.CSSProperties
       }
     >
--- src/app/publisher/[...publisherId]/page.tsx
+++ src/app/publisher/[...publisherId]/page.tsx
@@ -1,16 +1,18 @@
-import Publisher2 from '@/app/the-bump/page';
-import type { Metadata } from 'next';
-import publisherMetaData from '../../../../Data/metaData.json';
+import Publisher2 from "@/app/the-bump/page";
+import type { Metadata } from "next";
+import publisherMetaData from "../../../../Data/metaData.json";
 
 export async function generateMetadata({ params }): Promise<Metadata> {
   const publisher = await params;
-  const publisherName = publisher['publisherId'][0];
+  const publisherName = publisher["publisherId"][0];
   const metaData = publisherMetaData[publisherName] ?? {};
   return {
     ...metaData,
   };
 }
 
-export default function page() {
-  return <Publisher2 publisher="" />;
+export default async function page({ params }) {
+  const publisher = await params;
+  const publisherName = publisher["publisherId"][0];
+  return <Publisher2 publisher={publisherName} />;
 }
