d7210efd2cb206bdf15fe7fa0f11af248a4ffd13
Amit Negi <amit.negi077@gmail.com>
2025-02-28 18:24:25 +0530

    meta tweaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,8 @@ export const metadata = {
   title: 'Dpanda - plug n play white label e-commerce distribution platform',
   description:
     'Dpanda is a distribution platform for d2c brands, offers plug n play white label e-commerce solutions for audience monetisation of websites and mobile apps',
+  keywords:
+    'AI-based product recommendations, Seamless e-commerce integration, white label e-commerce solution for websites and apps, d2c distribution platform, monetise website and app traffic, advertising widgets for d2c brands, d2c campaign aggregation',
 };
 
 export default function RootLayout({ children }) {
