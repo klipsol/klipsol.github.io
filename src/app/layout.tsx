ca4dd6cbbaabc174418f4be399c24971f1eb4294
virudp <viru@DESKTOP-20T58A4>
2025-05-20 18:59:53 +0530

    add og tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata = {
     'Dpanda is a distribution platform for d2c brands, offers plug n play white label e-commerce solutions for audience monetisation of websites and mobile apps',
   keywords:
     'AI-based product recommendations, Seamless e-commerce integration, white label e-commerce solution for websites and apps, d2c distribution platform, monetise website and app traffic, advertising widgets for d2c brands, d2c campaign aggregation',
-  image: "https://www.dpanda.ai/DpandaLogo.png",
+  image: "https://www.dpanda.ai/dpanda.jpg",
   url: "https://www.dpanda.ai",
 };
 
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="dpanda" />
         <meta name="twitter:title" content={metadata.title} />
         <meta name="twitter:description" content={metadata.description} />
         <meta name="twitter:image" content={metadata.image} />
