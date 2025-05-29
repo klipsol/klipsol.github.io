1d6d01cd1132c52f4a898569407f957dadcd4086
Yadav <amarjit.yadav@dpanda.in>
2025-05-29 14:31:35 +0530

    added page for scratch card

--- src/app/Data/publishers.ts
+++ src/app/Data/publishers.ts
@@ -1,29 +1,29 @@
 const buyWidgetSites = [
-  { name: 'nymag', url: 'https://nymag.dpanda-buy-widget.pages.dev/' },
-  { name: 'buzzfeed', url: 'https://buzzfeed.dpanda-buy-widget.pages.dev/' },
-  { name: 'gq', url: 'https://gq.dpanda-buy-widget.pages.dev/' },
-  { name: 'fandom', url: 'https://fandom.dpanda-buy-widget.pages.dev/' },
-  { name: 'techradar', url: 'https://techradar.dpanda-buy-widget.pages.dev/' },
-  { name: 'people', url: 'https://people.dpanda-buy-widget.pages.dev/' },
-  { name: 'tmb', url: 'https://tmb.dpanda-buy-widget.pages.dev/' },
-  { name: 'dpanda', url: 'https://dpanda-us.dpanda-buy-widget.pages.dev/' },
-  { name: 'dpanda-in', url: 'https://master.dpanda-buy-widget.pages.dev/' },
-  { name: 'reddit', url: 'https://reddit.dpanda-buy-widget.pages.dev/' },
-  { name: 'pinterest', url: 'https://pinterest.dpanda-buy-widget.pages.dev/' },
-  { name: 'theBump', url: 'https://the-bump.dpanda-buy-widget.pages.dev' },
-  { name: 'the-bump', url: 'https://the-bump.dpanda-buy-widget.pages.dev' },
+  { name: "nymag", url: "https://nymag.dpanda-buy-widget.pages.dev/" },
+  { name: "buzzfeed", url: "https://buzzfeed.dpanda-buy-widget.pages.dev/" },
+  { name: "gq", url: "https://gq.dpanda-buy-widget.pages.dev/" },
+  { name: "fandom", url: "https://fandom.dpanda-buy-widget.pages.dev/" },
+  { name: "techradar", url: "https://techradar.dpanda-buy-widget.pages.dev/" },
+  { name: "people", url: "https://people.dpanda-buy-widget.pages.dev/" },
+  { name: "tmb", url: "https://tmb.dpanda-buy-widget.pages.dev/" },
+  { name: "dpanda", url: "https://dpanda-us.dpanda-buy-widget.pages.dev/" },
+  { name: "dpanda-in", url: "https://master.dpanda-buy-widget.pages.dev/" },
+  { name: "reddit", url: "https://reddit.dpanda-buy-widget.pages.dev/" },
+  { name: "pinterest", url: "https://pinterest.dpanda-buy-widget.pages.dev/" },
+  { name: "theBump", url: "https://the-bump.dpanda-buy-widget.pages.dev" },
+  { name: "the-bump", url: "https://the-bump.dpanda-buy-widget.pages.dev" },
   {
-    name: 'foodnetwork',
-    url: 'https://warner-bros.dpanda-buy-widget.pages.dev',
+    name: "foodnetwork",
+    url: "https://warner-bros.dpanda-buy-widget.pages.dev",
   },
   {
-    name: 'bestproducts',
-    url: 'https://hearst.dpanda-buy-widget.pages.dev/',
+    name: "bestproducts",
+    url: "https://hearst.dpanda-buy-widget.pages.dev/",
   },
 ];
 
 export const getWidgetUrl = (siteName) => {
-  const normalizedName = siteName.toLowerCase();
+  const normalizedName = siteName?.toLowerCase();
 
   const site = buyWidgetSites.find(
     (s) => s.name.toLowerCase() === normalizedName
