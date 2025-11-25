f59b7ea591780219f0a06794dd13c1d6f1a89c8f
Yadav <amarjit.yadav@dpanda.in>
2025-11-25 16:16:03 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,10 @@ export const metadata = {
     "Dpanda is a distribution platform for d2c brands, offers plug n play white label e-commerce solutions for audience monetisation of websites and mobile apps",
   keywords:
     "AI-based product recommendations, Seamless e-commerce integration, white label e-commerce solution for websites and apps, d2c distribution platform, monetise website and app traffic, advertising widgets for d2c brands, d2c campaign aggregation",
-  image: "https://www.bluecurrentlabs.com/fav icon.png",
+  image: "https://www.bluecurrentlabs.com/favicon.png",
   url: "https://www.bluecurrentlabs.com/",
   icons: {
-    icon: "https://www.bluecurrentlabs.com/fav icon.png",
+    icon: "https://www.bluecurrentlabs.com/favicon.png",
   },
 };
 
